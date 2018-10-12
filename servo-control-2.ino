1a29a18553c5580252f7b2c44ce1fadcb1c68fdb
Dante Dam <37320033+laggycomputer@users.noreply.github.com>
2018-10-12 13:30:44 -0700

    Added some servo functionality in form of comments

--- servo-control-2.ino
+++ servo-control-2.ino
@@ -1,20 +1,25 @@
+//#include <Servo.h>
 const int manualPin = 10;
 const int neutralPin = 11;
 const int autoPin = 12;
 const int buttonPin = 13;
+const int knobPin = "A0";
+//const int servoPin=0;
 int state = 1;
 int buttonState;
 int pressProcessed = false;
-//1-3 on state should be man, neutral, and auto
+int knobVal;
+//Servo servo;
 void setup() {
-  // put your setup code here, to run once:
   pinMode(manualPin, OUTPUT);
   pinMode(neutralPin, OUTPUT);
   pinMode(autoPin, OUTPUT);
   pinMode(buttonPin, INPUT);
+  pinMode(knobPin, INPUT);
   digitalWrite(manualPin, LOW);
   digitalWrite(neutralPin, LOW);
   digitalWrite(autoPin, LOW);
+  //servo.attach(servoPin);
 }
 void fade(int pin) {
   for (int fadeValue = 0; fadeValue <= 255; fadeValue += 5) {
@@ -27,8 +32,9 @@ void fade(int pin) {
   }
 }
 void loop() {
-  // put your main code here, to run repeatedly:
   buttonState = digitalRead(buttonPin);
+  knobVal=analogRead(knobPin);
+  knobVal = map(knobVal, 0, 1023, 0, 180);
   if (buttonState == LOW) {
     if (pressProcessed == false) {
       state++;
@@ -45,7 +51,7 @@ void loop() {
     fade(manualPin);
     digitalWrite(manualPin, HIGH);
     digitalWrite(autoPin, LOW);
-
+    //servo.write(knobVal);
   }
   if (state == 2) {
     fade(neutralPin);
@@ -56,6 +62,10 @@ void loop() {
     fade(autoPin);
     digitalWrite(neutralPin, LOW);
     digitalWrite(autoPin, HIGH);
+    /*servo.write(180);
+    delay(100);
+    servo.write(0);
+    */
   }
   delay(100);
 }
