72e75aba2528fe471431e54cd4bc077a3d997b77
Dante <37320033+laggycomputer@users.noreply.github.com>
2018-11-02 10:53:51 -0700

    bug fixes

--- servo-control-2.ino
+++ servo-control-2.ino
@@ -4,11 +4,13 @@ const int neutralPin = 11;
 const int autoPin = 12;
 const int buttonPin = 13;
 const int knobPin = A0;
-const int servoPin=0;
+const int servoPin = 9;
 int state = 1;
 int buttonState;
 int pressProcessed = false;
 int knobVal;
+int spinDir = 1;
+int servopos = 0;
 Servo servo;
 void setup() {
   Serial.begin(9600);
@@ -23,36 +25,33 @@ void setup() {
   servo.attach(servoPin);
 }
 void fade(int pin) {
-  for (int fadeValue = 0; fadeValue <= 255; fadeValue += 5) {
+  /*
+    for (int fadeValue = 0; fadeValue <= 255; fadeValue += 5) {
     analogWrite(pin, fadeValue);
     delay(15);
-  }
-  for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
+    }
+    for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
     analogWrite(pin, fadeValue);
     delay(15);
-  }
+    }
+  */
+  return;
 }
 void loop() {
   buttonState = digitalRead(buttonPin);
-  knobVal=analogRead(knobPin);
+  knobVal = analogRead(knobPin);
   knobVal = map(knobVal, 0, 1023, 0, 180);
-  if (buttonState == LOW) {
-    if (pressProcessed == false) {
+  if (buttonState != digitalRead(buttonPin)) {
+    if (buttonState == LOW) {
       state++;
-      if (state == 4) {
-        state = 1;
-      }
-      pressProcessed = true;
     }
-  }
-  else {
-    pressProcessed = false;
   }
   if (state == 1) {
     fade(manualPin);
     digitalWrite(manualPin, HIGH);
     digitalWrite(autoPin, LOW);
-    servo.write(knobVal);
+    servopos = knobVal;
+    servo.write(servopos);
   }
   if (state == 2) {
     fade(neutralPin);
@@ -63,13 +62,27 @@ void loop() {
     fade(autoPin);
     digitalWrite(neutralPin, LOW);
     digitalWrite(autoPin, HIGH);
-    servo.write(180);
-    delay(100);
-    servo.write(0);
-    
+    if (spinDir == 1) {
+      servopos++;
+      if (servopos > 180) {
+        servopos = 180;
+        spinDir = 0;
+      }
+    }
+    if (spinDir == 0) {
+      servopos--;
+      if (servopos < 0) {
+        servopos = 0;
+        spinDir = 1;
+      }
+    }
+    servo.write(servopos);
+  }
+  if (state == 4) {
+    state = 1;
   }
   Serial.print("State is ");
   Serial.print(state);
   Serial.print("\n\r");
-  delay(100);
+  delay(10);
 }
